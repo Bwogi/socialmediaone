ad4b8dd2adb90748a9473dcb858453b28037a998
Bwogi Andrew <andrew.bwogi@gmail.com>
2022-03-22 22:45:41 -0700

    added authentication using express-validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,16 +2,30 @@
 
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator')
+
 
 // @route   GET api/users
 // @desc    Test route
 // @access  Public
 router.get('/', (req, res) => res.send('User Route...'))
 
+
 // @route   POST api/users
 // @desc    Used to Register User (add Users to the database)
 // @access  Public
-router.post('/', (req, res) =>{
+router.post('/',[
+    check('name', 'Name is required').not().isEmpty(),
+    check('email','Please enter a valid email address').isEmail(),
+    check('password','Please enter the password of 6 or more characters').isLength({min:6})
+], (req, res) =>{
+    // We then check for validation errors
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()}); // bad request
+    }
+
+
     //lets check requests sent in the url body
     console.log(req.body);
     res.send('User Route...');
